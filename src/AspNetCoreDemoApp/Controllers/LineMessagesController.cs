ab76c3e14595ce679d2e5f96935944d891da18c9
jerryBC <dtbinhqb@gmail.com>
2017-10-30 07:16:20 +0000

    first commit

--- src/AspNetCoreDemoApp/Controllers/LineMessagesController.cs
+++ src/AspNetCoreDemoApp/Controllers/LineMessagesController.cs
@@ -35,57 +35,56 @@ namespace AspNetCoreDemoApp.Controllers
 			}
 			
 
-			// Line may send multiple events in one message, so need to handle them all.
-			foreach (Event lineEvent in activity.Events)
-			{
-				LineMessageHandler handler = new LineMessageHandler(lineEvent);
+			using (HttpClient client = GetClient())
+            {
+                using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+                {
+                    //JsonSerializerSettings settings = new JsonSerializerSettings()
+                    //{
+                    //    ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                    //    NullValueHandling = NullValueHandling.Ignore
+                    //};
 
-				Profile profile = await handler.GetProfile(lineEvent.Source.UserId);
 
-				switch (lineEvent.Type)
-				{
-					case EventType.Beacon:
-						await handler.HandleBeaconEvent();
-						break;
-					case EventType.Follow:
-						await handler.HandleFollowEvent();
-						break;
-					case EventType.Join:
-						await handler.HandleJoinEvent();
-						break;
-					case EventType.Leave:
-						await handler.HandleLeaveEvent();
-						break;
-					case EventType.Message:
-						Message message = JsonConvert.DeserializeObject<Message>(lineEvent.Message.ToString());
-						switch (message.Type)
-						{
-							case MessageType.Text:
-								await handler.HandleTextMessage();
-								break;
-							case MessageType.Audio:
-							case MessageType.Image:
-							case MessageType.Video:
-								await handler.HandleMediaMessage();
-								break;
-							case MessageType.Sticker:
-								await handler.HandleStickerMessage();
-								break;
-							case MessageType.Location:
-								await handler.HandleLocationMessage();
-								break;
-						}
-						break;
-					case EventType.Postback:
-						await handler.HandlePostbackEvent();
-						break;
-					case EventType.Unfollow:
-						await handler.HandleUnfollowEvent();
-						break;
-				}
-			}
 
+
+
+
+
+
+
+
+
+                    //settings.Converters.Add(new StringEnumConverter(true));
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    //StringContent content = new StringContent(
+                    //    JsonConvert.SerializeObject(reader.ReadToEndAsync(), settings),
+                    //    Encoding.UTF8, "application/json");
+                    //var result = await client.PostAsync("https://13.113.36.152/api/LineMessages", content);
+                    StringContent content = new StringContent(
+                        JsonConvert.SerializeObject(activity),
+                        Encoding.UTF8, "application/json");
+						
+                        await client.PostAsync("https://13.113.36.152/api/LineMessages", content);
+						
+                    
+                }
+            }
 			return new HttpResponseMessage(HttpStatusCode.OK);
+
+			
 		}
 
 		private async Task<bool> VaridateSignature(HttpRequestMessage request)
