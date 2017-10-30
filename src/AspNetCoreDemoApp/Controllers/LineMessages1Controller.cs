c500d9ac36f07c7a3294f2b9ac19a03d38b0bf26
jerryBC <dtbinhqb@gmail.com>
2017-10-30 06:21:24 +0000

    first commit

--- src/AspNetCoreDemoApp/Controllers/LineMessages1Controller.cs
+++ src/AspNetCoreDemoApp/Controllers/LineMessages1Controller.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net;
 using Newtonsoft.Json;
 using System.Security.Cryptography;
 using System.Text;
-
-
+using Microsoft.AspNetCore.Http;
+using AspNetCoreDemoApp.Models;
 using System.IO;
 
 namespace AspNetCoreDemoApp.Controllers
